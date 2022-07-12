2adc851d9244d2141c6ddf1c3ca1e2caea2473c4
Vitor Afonso <vittokm@hotmail.com>
2022-07-12 19:29:37 +0100

    added query active to  getallitems in index routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,15 +1,11 @@
 // jshint esversion:9
-
 const router = require('express').Router();
 const mongoose = require('mongoose');
-const User = require('../models/User.model');
 const Item = require('../models/Item.model');
-
-/************************** HOME *********************************/
 
 router.get('/', async (req, res, next) => {
   try {
-    const response = await Item.find();
+    const response = await Item.find({ active: true });
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ message: 'Algo correu mal ao carregar os items da base de dados:', error });
